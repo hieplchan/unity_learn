0a3dddd9a44f6a5033a8413d233e2d3b0185a501
Hiep Chan <hiep.lchan@gmail.com>
2024-09-19 11:49:13 +0700

    add sample runtime test

--- SoftwareReview/Assets/Scripts/Tests/Runtime.Tests/CoinCollectionRuntimeTests.cs
+++ SoftwareReview/Assets/Scripts/Tests/Runtime.Tests/CoinCollectionRuntimeTests.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -15,9 +18,31 @@ public class CoinCollectionRuntimeTests
     }
 
     [Test]
+    [LoadScene("Assets/Scenes/SampleScene.unity")]
     public void VerifyScene()
     {
         var go = GameObject.Find("CoinComponent");
         Assert.That(go, Is.Not.Null, "CoinComponent not found in {0}", SceneManager.GetActiveScene().path);
+    }
+}
+
+public class LoadSceneAttribute : NUnitAttribute, IOuterUnityTestAction
+{
+    private string scene;
+    
+    public LoadSceneAttribute(string scene)
+    {
+        this.scene = scene;
+    }
+    
+    public IEnumerator BeforeTest(ITest test)
+    {
+        Debug.Assert(scene.EndsWith(".unity"), "Scene must end with .unity");
+        yield return EditorSceneManager.LoadSceneInPlayMode(scene, new LoadSceneParameters(LoadSceneMode.Single));
+    }
+
+    public IEnumerator AfterTest(ITest test)
+    {
+        yield return null;
     }
 }
